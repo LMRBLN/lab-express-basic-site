77fa37a84d832a584d25111e61899a60011a6754
Lara Rockenstein <lara.rockenstein@live.de>
2022-07-05 23:51:37 +0200

    some more styling in telekom colors... true fans are going to love <3

--- app.js
+++ app.js
@@ -5,22 +5,18 @@ const app = express();
 app.use(express.static('public'));
 
 app.get("/", (request, response) => {
-    console.log("we've received a request for the HOME PAGE")
     response.sendFile(__dirname + '/views/home.html')
 });
 
 app.get("/about", (request, response) => {
-    console.log("we've received a request for the ABOUT PAGE")
     response.sendFile(__dirname + '/views/about.html')
 });
 
 app.get("/quotes", (request, response) => {
-    console.log("we've received a request for the QUOTES PAGE")
     response.sendFile(__dirname + '/views/quotes.html')
 });
 
 app.get("/gallery", (request, response) => {
-    console.log("we've received a request for the GALLERY PAGE")
     response.sendFile(__dirname + '/views/gallery.html')
 });
 
