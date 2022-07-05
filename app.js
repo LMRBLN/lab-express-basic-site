bad11473f9749a4c6f1db8ebfb973b204d5725d6
Lara Rockenstein <lara.rockenstein@live.de>
2022-07-05 17:34:54 +0200

    basic structure and home-page content are ready

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (request, response) => {
+    console.log("we've received a request for the HOME PAGE")
+    response.sendFile(__dirname + '/views/home.html')
+});
+
+app.get("/about", (request, response) => {
+    console.log("we've received a request for the ABOUT PAGE")
+    response.sendFile(__dirname + '/views/about.html')
+});
+
+app.get("/quotes", (request, response) => {
+    console.log("we've received a request for the QUOTES PAGE")
+    response.sendFile(__dirname + '/views/quotes.html')
+});
+
+app.listen(3001, () => console.log("express app listening in port 3001"));
